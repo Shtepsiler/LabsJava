93573cdf893cf7336953f40c21a6a9c196e09c98
Shtepsiler <100515332+Shtepsiler@users.noreply.github.com>
2023-04-13 00:49:14 +0300

    lab8

--- lab8/lab8/src/Main.java
+++ lab8/lab8/src/Main.java
@@ -2,15 +2,19 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 class LoginWindow extends JFrame {
     private JLabel inLabel, outLabel;
     private JTextField inField, outField;
-    private JButton copyButton;
+    private JButton copyButton, prarrButton;
 
     public LoginWindow() {
+        ArrayList<String> listofinners = new ArrayList<>();
+
+
         // Налаштування параметрів вікна
-        setTitle("Копіювання");
+        setTitle("Авторизація");
         setSize(300, 150);
         setLocationRelativeTo(null);
         setResizable(false);
@@ -22,7 +26,7 @@ class LoginWindow extends JFrame {
         outLabel = new JLabel("поле виводу:");
         outField = new JTextField(20);
         copyButton = new JButton("Скопіювати");
-
+        prarrButton = new JButton("Друкувати");
 
 
         JPanel panel = new JPanel(new GridBagLayout()); // використання GridBagLayout для настройки компонентів
@@ -50,18 +54,31 @@ class LoginWindow extends JFrame {
         constraints.gridwidth = 2;
         panel.add(outField, constraints);
 
-        constraints.gridx = 1;
+        constraints.gridx = 0;
         constraints.gridy = 2;
         constraints.gridwidth = 1;
         constraints.anchor = GridBagConstraints.LINE_END;
         panel.add(copyButton, constraints);
 
+        constraints.gridx = 1;
+        constraints.gridy = 2;
+        constraints.gridwidth = 1;
+        constraints.anchor = GridBagConstraints.LINE_END;
+        panel.add(prarrButton, constraints);
+
         copyButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 // Отримання тексту з текстового поля і збереження його в локальну змінну
                 String text = inField.getText();
-                System.out.println("Введений текст: " + text);
                 outField.setText(text);
+                listofinners.add(text);
+            }
+        });
+        prarrButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                for(String inner : listofinners)
+                System.out.println(inner);
             }
         });
 
